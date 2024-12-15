d75b48f79ae9b87888f3725494b5cb9edf38badd
katohs <s_kato@toridori.co.jp>
2024-12-15 12:21:08 +0900

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
-import { LoggerMiddleware } from "./access-log";
-import { PrismaModule } from "./db/prisma.module";
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { LoggerMiddleware } from './access-log'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './db/prisma.module'
 
 @Module({
   imports: [PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(LoggerMiddleware).forRoutes("*");
+    consumer.apply(LoggerMiddleware).forRoutes('*')
   }
 }
